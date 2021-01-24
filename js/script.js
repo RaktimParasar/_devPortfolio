ba0235abfc5ea973aeab9f5bc5b9008503ef69c8
RaktimParasar <raktimparasar96@gmail.com>
2021-01-24 11:07:23 +0530

    Update media queries

--- js/script.js
+++ js/script.js
@@ -1,80 +1,80 @@
-//explore buttonScroll
-document.getElementById("explore").addEventListener("click", () => {
-  var elmnt = document.getElementById("tech_stack");
-  elmnt.scrollIntoView();
-});
+// //explore buttonScroll
+// document.getElementById("explore").addEventListener("click", () => {
+//   var elmnt = document.getElementById("about_me");
+//   elmnt.scrollIntoView();
+// });
 
-//animation
-gsap.registerPlugin(ScrollTrigger);
-var t1 = gsap.timeline();
+// //animation
+// gsap.registerPlugin(ScrollTrigger);
+// var t1 = gsap.timeline();
 
-t1.from(".hero-text-container", {
-  y: "-30%",
-  opacity: 0,
-  duration: 2,
-  ease: Power4.easeout,
-});
+// t1.from(".hero-text-container", {
+//   y: "-30%",
+//   opacity: 0,
+//   duration: 2,
+//   ease: Power4.easeout,
+// });
 
-t1.from(
-  ".stagger_1",
-  {
-    opacity: 0,
-    y: "-50%",
-    stagger: 0.3,
-    ease: Power4.easeout,
-    duration: 2,
-  },
-  "-=1.5"
-);
+// t1.from(
+//   ".stagger_1",
+//   {
+//     opacity: 0,
+//     y: "-50%",
+//     stagger: 0.3,
+//     ease: Power4.easeout,
+//     duration: 2,
+//   },
+//   "-=1.5"
+// );
 
-t1.from(
-  ".arrow",
-  {
-    opacity: 0,
-    x: "-50",
-    stagger: 0.3,
-    ease: "elastic.out(1, 0.3)",
-    duration: 2.5,
-  },
-  "-=1.5"
-);
+// t1.from(
+//   ".arrow",
+//   {
+//     opacity: 0,
+//     x: "-50",
+//     stagger: 0.3,
+//     ease: "elastic.out(1, 0.3)",
+//     duration: 2.5,
+//   },
+//   "-=1.5"
+// );
 
-gsap.from(".hero-image", {
-  stagger: 0.1,
-  scale: 0.5,
-  duration: 1,
-  ease: "back.out(1.7)",
-});
+// gsap.from(".hero-image", {
+//   stagger: 0.1,
+//   scale: 0.5,
+//   duration: 1,
+//   ease: "back.out(1.7)",
+// });
 
-gsap.from(".transition_up", {
-  scrollTrigger: {
-    trigger: ".transition_up",
-    start: "top bottom",
-  },
-  y: 50,
-  opacity: 0,
-  duration: 1.2,
-  stagger: 0.3,
-});
+// gsap.from(".transition_up", {
+//   scrollTrigger: {
+//     trigger: ".transition_up",
+//     start: "top bottom",
+//   },
+//   y: 50,
+//   opacity: 0,
+//   duration: 1.2,
+//   stagger: 0.3,
+// });
 
-gsap.from(".transition_right", {
-  scrollTrigger: {
-    trigger: ".transition_right",
-    start: "top bottom",
-  },
-  x: 50,
-  opacity: 0,
-  duration: 1.2,
-  stagger: 0.6,
-});
+// gsap.from(".transition_right", {
+//   scrollTrigger: {
+//     trigger: ".transition_right",
+//     start: "top bottom",
+//   },
+//   x: 50,
+//   opacity: 0,
+//   duration: 1.2,
+//   stagger: 0.6,
+// });
 
-gsap.from(".transition_left", {
-  scrollTrigger: {
-    trigger: ".transition_left",
-    start: "top bottom",
-  },
-  y: -50,
-  opacity: 0,
-  duration: 1.2,
-  stagger: 0.6,
-});
+// gsap.from(".transition_left", {
+//   scrollTrigger: {
+//     trigger: ".transition_left",
+//     start: "top bottom",
+//   },
+//   y: -50,
+//   opacity: 0,
+//   duration: 1.2,
+//   stagger: 0.6,
+// });
